6d08a5919d5853491e5887dfd0f7905d0a76145d
Yurii <yura.onyshchuk@gmail.com>
2022-01-18 22:38:32 +0200

    Created Task and Category models

--- ToDoProject/main_app/models.py
+++ ToDoProject/main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField('Назва', max_length=100)
+    text = models.TextField('Опис')
+    created_data = models.DateTimeField('Срок виконання')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    class Meta:
+        verbose_name = 'Завдання'
+        verbose_name_plural = 'Завдання'
+
+    def __str__(self):
+        return f'{self.title}, {self.title}'
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, db_index=True)
+
+    class Meta:
+        verbose_name = 'Категорія'
+        verbose_name_plural = 'Категорії'
