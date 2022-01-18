0cc20437d2d403ad4f64517be77e5436dea1008b
Yurii <yura.onyshchuk@gmail.com>
2022-01-18 23:48:05 +0200

    Home page with task list

--- ToDoProject/main_app/views.py
+++ ToDoProject/main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from .models import Task
 
 
 def index(request):
-    return render(request, 'main_app/index.html')
+    task_list = Task.objects.all()
+    return render(request, 'main_app/index.html', {'task_list': task_list})
